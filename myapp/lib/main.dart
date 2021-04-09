e4f98449d6a53de87f50bca717081b00ed6343b9
THOMAS BLANCO <thomas1a1blanco@gmail.com>
2021-04-08 23:58:56 -0300

    reverse

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generatorrr',
+      title: 'Startup Name Generator',
       theme: ThemeData(
       primaryColor: Colors.white,
    
